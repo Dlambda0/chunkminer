5f5ebf54941216b95f2bbac254a4015ce9625b15
dlambda <20195teel@gmail.com>
2024-06-18 22:06:54 -0400

    ?

--- src/main.py
+++ src/main.py
@@ -1,18 +1,15 @@
-import pyautogui
+import pyautogui as pag
 import cv2 as cv
 
-#used to determine what tool for the job
 class item:
     def __init__(self, quality, tool):
         self.quality = quality
         self.type = tool 
-    def identify():
-        pass
-
+    def tool_identify():
+        if cv.imread("woodpick.pick"):
+            
+        
 def chunksize():
-    pass
-
-def toolpicker():
     pass
 
 def autominer():
